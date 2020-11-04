a45f60ebd337a87da271b55a9763a91d1c42b94b
Skylar <rorelei@gmail.com>
2020-11-03 21:22:34 -0800

    Update RemoveKthFromTheLastElement.cs

--- RemoveKthFromTheLastElement.cs
+++ RemoveKthFromTheLastElement.cs
@@ -13,112 +13,82 @@ public class LinkedListNode
 
 public class MyLinkedList
 {
-public MyLinkedList()
-    {
-      Head = null;
-      Count = 0;
-    }
+	public MyLinkedList()
+	{
+		Head = null;
+		Count = 0;
+	}
     
-    public LinkedListNode Head { get; private set; }
+	public LinkedListNode Head { get; private set; }
+
+	public int Count { get; private set; }
     
-    public int Count { get; private set; }
-    
-    public void Add(LinkedListNode node)
-    {
-      if (Head == null)
-      {
-        Head = node;
-      }
-      else
-      {
-        var current = Head;
-        
-        while (current.Next != null)
-        {
-          current = current.Next;
-        }
-        
-        current.Next = node;
-      }
-      
-      Count++;
-    }
-    
-    public void Remove(int index)
-    {
-      if (index >= Count)
-      {
-        throw new InvalidOperationException("The index needs to be smaller than the size of the linked list.");
-      }
-      
-      var current = Head;
-      
-      for (int i = 0; i < index - 1; i++)
-      {
-        current = current.Next;
-      }
-      
-      current.Next = current.Next.Next;
-      Count--;
-    }
-  }
+	public void Add(LinkedListNode node)
+	{
+		if (Head == null)
+		{
+			Head = node;
+		}
+		else
+		{
+			var current = Head;
+
+			while (current.Next != null)
+			{
+  				current = current.Next;
+			}
+
+			current.Next = node;
+		}
+
+		Count++;
+	}
+
+	public void Remove(int index)
+	{
+		if (index >= Count)
+		{
+			throw new InvalidOperationException("The index needs to be smaller than the size of the linked list.");
+		}
+
+		var current = Head;
+
+		for (int i = 0; i < index - 1; i++)
+		{
+			current = current.Next;
+		}
+
+		current.Next = current.Next.Next;
+		Count--;
+	}
+}
   
 public static class LinkedListHelpers
 {
-	public static void RemoveDuplicates(LinkedListNode n)
+	public static void PrintLinkedList(MyLinkedList linkedList)
 	{
-		var hashSet = new HashSet<int>();
-		LinkedListNode previous = null;
+		var node = linkedList.Head;
 
-		while (n != null)
+		while (node != null)
 		{
-			if (hashSet.Contains(n.Data))
-			{
-				previous.Next = n.Next;
-			}
-			else
-			{
-                hashSet.Add(n.Data);
-                previous = n;
-            }
+			Console.Write(node.Data + " -> ");
+			node = node.Next;
+		}
 
-            n = n.Next;
-        }
-    }
+		Console.Write("null");
+		Console.WriteLine();
+	}
 
-    public static void RemoveDuplicatesWithoutBuffer(LinkedListNode n)
-    {
-        var current = n;
-        
-        while (current != null)
-        {
-            var runner = current;
-            
-            while (runner.Next != null)
-            {
-                if (runner.Next.Data == current.Data)
-                {
-                    runner.Next = runner.Next.Next; 
-                }
-                else
-                {
-                    runner = runner.Next;
-                }
-            }
-            
-            current = current.Next;
-        }
-    }
-    
-    public static void PrintLinkedList(LinkedListNode n)
-    {
-        while (n != null)
-        {
-            Console.Write(n.Data + " -> ");
-            n = n.Next;
-        }
+	public static void RemoveKthToTheLastElement(MyLinkedList linkedList, int k)
+	{
+		if (k >= linkedList.Count)
+		{
+			throw new InvalidOperationException("k should be smaller than the size of the linked list.");
+		}
 
-        Console.Write("null");
-        Console.WriteLine();
-    }
+		// If say Count = 5 and we want to remove 4th to the last element, we're actually removing 5-4 = 1st element.
+		var index = linkedList.Count - k;
+
+		linkedList.Remove(index);
+	}
 }
